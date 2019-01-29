4af331b85d9db9f55353299d91e6afb7b4581eca
JRussell2001 <468949@chrome.sussexdowns.ac.uk>
2019-01-29 20:23:53 +0000

    Walls and a chest added (No bugs or errors)

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed;
+
+    private Rigidbody rb;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertival = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertival);
+
+        rb.AddForce(movement * 10);
+    }
+
+}
+
+
